7dca5a245e44b9afa5edb044f6de67e575fab698
Ystrouzas <34077059+Ystrouzas@users.noreply.github.com>
2018-02-14 00:38:18 -0800

    Blog completed

---
+++ Startup Website/js/modal.js
@@ -0,0 +1,11 @@
+// JavaScript Document
+
+$(document).ready(function(){
+    // Show the Modal on load
+    $("#myModal").modal("show");
+    
+    // Hide the Modal
+    $("#myBtn").click(function(){
+        $("#myModal").modal("hide");
+    });
+});
